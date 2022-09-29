fb38d0491e20fc8dcb83acfc5ea98c33b916894f
John Doe <ovoshebazaqq@gmail.com>
2022-09-29 16:06:29 +0300

    first commit

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ async function createFolder() {
    // {headless:false}
   const browser = await puppeteer.launch({
       // headless:false,
-      executablePath: '/usr/bin/chromium-browser',
+      // executablePath: '/usr/bin/chromium-browser',
       args: ['--no-sandbox']
    });
   const page = await browser.newPage();
