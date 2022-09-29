a6505635f5d37553db1511dfec3db264883e9bd5
John Doe <ovoshebazaqq@gmail.com>
2022-09-29 16:41:10 +0300

    first commit

--- index.js
+++ index.js
@@ -246,10 +246,10 @@ async function createFolder() {
                // await page.waitForTimeout(10000)
                if (await page.$('.Management .ListItem.chat-item-clickable:nth-child(3)') !== null) {
                   await admin.click()
-                  await page.waitForTimeout(700)
+                  await page.waitForTimeout(1000)
                   // await page.waitForSelector('.icon-delete')
                   await page.click('.icon-delete')
-                  await page.waitForTimeout(700)
+                  await page.waitForTimeout(1000)
                   // await page.waitForSelector('.Button.confirm-dialog-button.default.danger.text')
                   page.click('.Button.confirm-dialog-button.default.danger.text')
                   await deleteAdmin()
